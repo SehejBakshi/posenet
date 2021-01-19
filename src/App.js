8e03a00db8d05c28bda45f7ad50b982bfc3610ed
OculusRift03 <sehej.bakshi@yahoo.in>
2021-01-19 19:43:30 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import * as tf from "@tensorflow/tfjs";
 import * as posenet from "@tensorflow-models/posenet";
 import Webcam from "react-webcam";
+import { drawKeypoints, drawSkeleton } from "./utilities";
 
 function App() {
   const webcamRef = useRef(null);
@@ -12,10 +13,44 @@ function App() {
   // Load Posenet
   const runPosenet = async () => {
     const net = await posenet.load({
-      inputResolution:{width:640, height:480},
-      scale:0.5
-    })
-  }
+      inputResolution: {width:640, height:480},
+      scale:0.5,
+    });
+
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef.current.video.readyState===4) {
+      // Get video properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth
+      webcamRef.current.video.height = videoHeight
+
+      // Make Detections
+      const pose = await net.estimateSinglePose(video);
+      console.log(pose);
+
+      drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+    }
+  };
+
+  const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
+    const ctx = canvas.current.getContext("2d");
+    canvas.current.width = videoWidth;
+    canvas.current.height = videoHeight;
+
+    drawKeypoints(pose["keypoints"], 0.5, ctx);
+    drawSkeleton(pose["keypoints"], 0.5, ctx);
+  };
+
+  runPosenet();
 
 
   return (
