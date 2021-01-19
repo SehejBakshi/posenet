17194aaa8b43a32785e08466ca01f8fb38fa8aba
OculusRift03 <sehej.bakshi@yahoo.in>
2021-01-19 19:06:36 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
+import React, {useRef} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as tf from "@tensorflow/tfjs";
+import * as posenet from "@tensorflow-models/posenet";
+import Webcam from "react-webcam";
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  // Load Posenet
+  const runPosenet = async () => {
+    const net = await posenet.load({
+      inputResolution:{width:640, height:480},
+      scale:0.5
+    })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">"
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position:"absolute",
+            marginLeft:"auto",
+            marginRight:"auto",
+            left:0,
+            right:0,
+            textAlign:"center",
+            zindex:9,
+            width:640,
+            height:480
+            }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position:"absolute",
+            marginLeft:"auto",
+            marginRight:"auto",
+            left:0,
+            right:0,
+            textAlign:"center",
+            zindex:9,
+            width:640,
+            height:480
+            }}
+        />
       </header>
     </div>
   );
